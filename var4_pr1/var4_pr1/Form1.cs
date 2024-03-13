172d35ea7b29d45681a2e005c28776079bd78d34
Remizov_Kirill <woron0987654321@gmail.com>
2024-03-13 13:47:03 +0300

    Готовая первая работа вариант 4

---
+++ var4_pr1/var4_pr1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using backend;
+
+namespace var4_pr1
+{
+    public partial class Form1 : Form
+    {
+        data Working = new data();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string faculty;
+            faculty = textBox1.Text;
+            Working.Get_Data_faculty(faculty);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string Deas_name;
+            Deas_name = textBox2.Text;
+            Working.Get_Data_Deas_name(Deas_name);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string Abbriviation;
+            Abbriviation = textBox3.Text;
+            Working.Get_Data_Abriviation(Abbriviation);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Working._Create_File();
+        }
+    }
+}
---
+++ var4_pr1/var4_pr1/backend.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace backend
+{
+    class data
+    {
+        private string Faculty;
+        private string FIO;
+        private string abrivature;
+        public void Get_Data_faculty(string _faculaty)
+        {
+            Faculty = _faculaty;
+        }
+        public void Get_Data_Deas_name(string _Deas_name)
+        {
+            FIO = _Deas_name;
+        }
+        public void Get_Data_Abriviation(string _abbriviation)
+        {
+            abrivature = _abbriviation;
+        }
+        public void _Create_File()
+        {
+            using (FileStream itog = new FileStream("C:\\Users\\woron\\source\\repos\\var4_pr1\\var4_pr1\\info.txt", FileMode.Append))
+            {
+                string Faculty_ = "\nFaculty: ";
+                string FIO_ = "Dean's Name: ";
+                string abrivature_ = "Abbriviation: ";
+                Faculty_ += Faculty;
+                Faculty_ += "\n";
+                FIO_ += FIO;
+                FIO_ += "\n";
+                abrivature_ += abrivature;
+                abrivature_ += "\n";
+                byte[] faculaty_array = System.Text.Encoding.Default.GetBytes(Faculty_);
+                byte[] FIO_array = System.Text.Encoding.Default.GetBytes(FIO_);
+                byte[] abriviature_array = System.Text.Encoding.Default.GetBytes(abrivature_);
+
+                itog.Write(faculaty_array, 0, faculaty_array.Length);
+                itog.Write(FIO_array, 0, FIO_array.Length);
+                itog.Write(abriviature_array, 0, abriviature_array.Length);
+            }
+        }
+    }
+}
